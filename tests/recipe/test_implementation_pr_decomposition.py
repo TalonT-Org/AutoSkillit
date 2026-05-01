e0b7cf4f698fa5f1e702d3d80adb775e41af96bc
Trecek <talontrecek@gmail.com>
2026-04-30 17:58:33 -0700

    refactor: change default value of arch_lenses to false in implementation and remediation recipes test: update tests to reflect arch_lenses default value change

--- tests/recipe/test_implementation_pr_decomposition.py
+++ tests/recipe/test_implementation_pr_decomposition.py
@@ -70,8 +70,8 @@ def test_arch_lenses_ingredient_declared(recipe):
     assert "arch_lenses" in recipe.ingredients
 
 
-def test_arch_lenses_defaults_to_true(recipe):
-    assert recipe.ingredients["arch_lenses"].default == "true"
+def test_arch_lenses_defaults_to_false(recipe):
+    assert recipe.ingredients["arch_lenses"].default == "false"
 
 
 def test_arch_lenses_is_not_hidden(recipe):

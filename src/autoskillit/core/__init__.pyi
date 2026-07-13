95b63bed948c438e95359e797a246fb4f3a332a7
Trecek <talontrecek@gmail.com>
2026-07-13 12:29:16 -0700

    fix: auto-fix pyi import ordering via ruff

--- src/autoskillit/core/__init__.pyi
+++ src/autoskillit/core/__init__.pyi
@@ -375,12 +375,12 @@ from .types import closure_authority_spec_from_args as closure_authority_spec_fr
 from .types import compute_remaining as compute_remaining
 from .types import extract_path_arg as extract_path_arg
 from .types import extract_positional_args as extract_positional_args
-from .types import parse_plan_paths as parse_plan_paths
 from .types import extract_skill_name as extract_skill_name
 from .types import fleet_error as fleet_error
 from .types import is_path_like_token as is_path_like_token
 from .types import is_valid_codex_model_id as is_valid_codex_model_id
 from .types import model_class as model_class
+from .types import parse_plan_paths as parse_plan_paths
 from .types import resolve_payload_field as resolve_payload_field
 from .types import resolve_skill_name as resolve_skill_name
 from .types import resolve_target_skill as resolve_target_skill

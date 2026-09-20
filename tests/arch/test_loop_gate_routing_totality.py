fbab4c0ea9da46b65103e3f20d7e02cf03bed231
Trecek <talontrecek@gmail.com>
2026-09-20 09:41:50 -0700

    style: wrap line in _LOOP_SITES to satisfy ruff E501

--- tests/arch/test_loop_gate_routing_totality.py
+++ tests/arch/test_loop_gate_routing_totality.py
@@ -26,8 +26,8 @@ _LOOP_SITES = {
     "implementation-groups": (
         "check_active_trigger_loop check_audit_integrity_retry check_ci_loop "
         "check_ci_post_queue_loop check_ci_rebase_loop check_ci_timed_out_loop "
-        "check_dequeue_retry_loop check_dirty_main_retry check_flake_loop check_group_plan_iteration "
-        "check_merge_fix_loop "
+        "check_dequeue_retry_loop check_dirty_main_retry check_flake_loop "
+        "check_group_plan_iteration check_merge_fix_loop "
         "check_merge_rebase_loop check_merge_test_fix_loop check_next_iteration "
         "check_ref_push_loop check_replan_iteration check_stall_loop check_test_fix_loop"
     ),
